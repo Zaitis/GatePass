38174ddb0186ed83435078f9553a69360bbb6a29
Kacper Bąk <contact@kacperbak.pl>
2023-07-04 19:26:16 +0200

    feat: security config

--- src/main/java/com/sk/GatePass/security/SecurityConfig.java
+++ src/main/java/com/sk/GatePass/security/SecurityConfig.java
@@ -8,29 +8,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig  extends VaadinWebSecurity {
+public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.authorizeRequests()
                 .requestMatchers("/images/*.png").permitAll();
 
         super.configure(http);
         setLoginView(http, LoginView.class);
+
+        http.formLogin()
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest request,
+                                                    HttpServletResponse response,
+                                                    Authentication authentication) throws IOException, ServletException {
+                    DefaultSavedRequest savedRequest =
+                            (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+
+                    if (authentication.getAuthorities().stream()
+                            .anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"))) {
+                        response.sendRedirect("/admin-dashboard");
+                    } else {
+                        response.sendRedirect("/dashboard");
+                    }
+                }
+            });
     }
 
     @Bean
     public UserDetailsService users() {
         UserDetails user = User.builder()
                 .username("user")
-                // password = password with this hash, don't tell anybody :-)
                 .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
                 .roles(String.valueOf(Role.USER))
                 .build();
@@ -40,7 +65,5 @@ public class SecurityConfig  extends VaadinWebSecurity {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
-
-
     }
 }
