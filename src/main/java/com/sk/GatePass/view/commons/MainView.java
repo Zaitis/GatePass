0bd5626435d7ea0a7be72c590a81d3c72c66fa77
Zaitis <krzysztof@painm.pl>
2023-07-12 21:45:46 +0200

    Some changes in main view

--- src/main/java/com/sk/GatePass/view/commons/MainView.java
+++ src/main/java/com/sk/GatePass/view/commons/MainView.java
@@ -2,10 +2,16 @@ package com.sk.GatePass.view.commons;
 
 
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 
@@ -15,9 +21,34 @@ public class MainView extends VerticalLayout {
 
 
     public MainView() {
-        RouterLink loginView = new RouterLink("Login", LoginView.class);
-        RouterLink createView = new RouterLink("Create Account", CreateAccountView.class);
-        add(loginView, createView);
+        setWidth("80%");
+        setAlignItems(Alignment.CENTER);
+        
+
+
+        H1 title = new H1("Welcome at Parking Manager");
+        H3 paragraphTitle = new H3("How it works?");
+        Paragraph paragraph = new Paragraph(
+                "The application allows users to create an account," +
+                        " choose the company they belong to, and add cars." +
+                        " For each car, the user can request only one gate pass." +
+                        " The administrators manage the application," +
+                        " and they have access to an administrative panel where they can add companies and accept" +
+                        " or reject gate pass requests."
+        );
+
+
+
+        Button loginView = new Button("Login");
+        Button createView = new Button("Create Account");
+        loginView.addClickListener(e -> UI.getCurrent().navigate(LoginView.class));
+        loginView.addClickListener(e -> UI.getCurrent().navigate(CreateAccountView.class));
+        Div spaceDiv = new Div();
+        spaceDiv.setWidth("600px");
+
+        HorizontalLayout header = new HorizontalLayout();
+        header.add(title, loginView, createView);
+        add(header, paragraphTitle, paragraph);
 
 
     }
